1ca710353d3b96597a356f736d7db07a9718407b
Pasca Vlad <pascavlad16@gmail.com>
2021-01-21 21:23:54 +0200

    added show alert, validation, clear alert, delete task

--- app.js
+++ app.js
@@ -24,6 +24,27 @@ UI.prototype.addBookToList = function (book) {
     list.appendChild(row);
 };
 
+UI.prototype.showAlert = function (message, className) {
+    const div = document.createElement("div");
+    const heading = document.querySelector("#heading");
+
+    div.className = `alert ${className}`;
+
+    div.appendChild(document.createTextNode(message));
+
+    heading.after(div);
+
+    setTimeout(function () {
+        document.querySelector(".alert").remove();
+    }, 2000);
+};
+
+UI.prototype.deleteBook = function (target) {
+    if (target.classList.contains("delete")) {
+        target.parentElement.parentElement.remove();
+    }
+};
+
 // Clear Fields Function
 UI.prototype.clearFields = function () {
     document.getElementById("title").value = "";
@@ -37,14 +58,31 @@ document.getElementById("book-form").addEventListener("submit", function (e) {
     const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
     const isbn = document.getElementById("ISBN").value;
+    // validation, the alert that goes when I don't fill the form
     // Instantiate a book
     const book = new Book(title, author, isbn);
     // Instantiate UI
     const ui = new UI();
-    // Add book to list
-    ui.addBookToList(book);
-    // Clear fields
-    ui.clearFields();
+    if (title === "" || author === "" || isbn === "") {
+        ui.showAlert("Please fill in all fields", "error");
+    } else {
+        // Add book to list
+        ui.addBookToList(book);
+        // Clear fields
+        ui.clearFields();
+
+        ui.showAlert("Book added to the list", "success");
+
+        e.preventDefault();
+    }
+});
+
+document.querySelector("#book-list").addEventListener("click", function (e) {
+    const ui = new UI();
+
+    ui.deleteBook(e.target);
+
+    ui.showAlert("Book deleted from the list", "success");
 
     e.preventDefault();
 });
